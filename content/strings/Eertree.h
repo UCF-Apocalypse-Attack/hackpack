c04244f4e45aa6cdb82870b77210c2875abbc169
Sachin S <ssk4988@gmail.com>
2024-01-31 00:43:09 -0500

    put eertree into struct

--- content/strings/Eertree.h
+++ content/strings/Eertree.h
@@ -8,23 +8,27 @@
 
 #pragma once
 
-vector<int> slink = {0, 0}, len = {-1, 0};
-vector<vector<int>> down(2, vector<int>(26, -1));
-int cur = 0, t = 0;
-for (int i = 0; i < str.size(); i++) {
-	char c = str[i]; int ci = c - 'a';
-	while (t <= 0 || str[t-1] != c) 
-	t = i - len[cur = slink[cur]];
-	if (down[cur][ci] == -1) {
-	down[cur][ci] = slink.size();
-	down.emplace_back(26, -1);
-	len.push_back(len[cur] + 2);
-	if (len.back() > 1) {
-		do t = i - len[cur = slink[cur]];
-		while(t <= 0 || str[t-1] != c);
-		slink.push_back(down[cur][ci]);
-	} else slink.push_back(1);
-	cur = slink.size() - 1;
-	} else cur = down[cur][ci];
-	t = i - len[cur] + 1;
-}
+struct Eertree {
+	vector<int> slink = {0, 0}, len = {-1, 0};
+	vector<vector<int>> down;
+	int cur = 0, t = 0;
+	Eertree(string &str) : down(2, vector<int>(26, -1)) {
+		for (int i = 0; i < str.size(); i++) {
+			char c = str[i]; int ci = c - 'a';
+			while (t <= 0 || str[t-1] != c) 
+			t = i - len[cur = slink[cur]];
+			if (down[cur][ci] == -1) {
+			down[cur][ci] = slink.size();
+			down.emplace_back(26, -1);
+			len.push_back(len[cur] + 2);
+			if (len.back() > 1) {
+				do t = i - len[cur = slink[cur]];
+				while(t <= 0 || str[t-1] != c);
+				slink.push_back(down[cur][ci]);
+			} else slink.push_back(1);
+			cur = slink.size() - 1;
+			} else cur = down[cur][ci];
+			t = i - len[cur] + 1;
+		}
+	}
+};
