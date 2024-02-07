0abfd8242422f8f53a06663f8087ed1b944e7bdc
Andy Phan <andytphan@outlook.com>
2024-02-07 16:02:04 -0500

    replace centroid decomp with code from lrvideckis

--- content/graphs/CentroidDecomp.h
+++ content/graphs/CentroidDecomp.h
@@ -1,30 +1,44 @@
 /**
- * Author: Andy Phan
- * Date: 2023-02-11
- * Description: Computes centroid decomposition on connected tree, and runs callback function
- * Usage: centroidDecomp(adj, [\&] (int root, vector<int>\& isIn) { ... });
-	all nodes with isIn[i] = 1 connected to root are part of root's centroid
+ * Author: Luke Videckis
+ * Date: 2024-01-08
+ * License: CC0
+ * Source: https://github.com/lrvideckis/programming_team_code/
+ * Description: Calls callback function on undirected forest for each centroid
+ * Usage: centroid(adj, [\&](const vector<vector<int>>\& adj, int cent) { \ldots\ });
  * Time: $O(n \log n)$
  * Status: Untested
  */
 #pragma once
 
-template<class G, class F>
-void centroidDecomp(G g, F f) {
-	vi s(sz(g), 1), par(sz(g)), is(s);
-	auto go = [&] (int u, int p, auto& go) -> void {
-		if ((par[u] = p) != -1) g[u].erase(find(all(g[u]), p));
-		for (int v : g[u]) go(v, u, go), s[u] += s[v];
-	};
-	go(0, -1, go); queue<int> q({0});
-	while (sz(q)) {
-		int x = q.front(), b = x, ss, c; q.pop();
-		do for(int v : g[c = b]) if(s[v] > s[x]/2) b = v;
-			while(c != b);
-		f(c, is);
-		is[c] = 0, ss = s[c];
-		for (int v : g[c]) if (s[v] > 0) q.push(v);
-		if (c != x) q.push(x);
-		do s[c] -= ss; while ((c = par[c]) != par[x]);
-	}
-}
+template <class F> struct centroid {
+    vector<vector<int>> adj;
+    F f;
+    vector<int> sub_sz;
+    centroid(const vector<vector<int>>& a_adj, F a_f)
+        : adj(a_adj), f(a_f), sub_sz(ssize(adj), -1) {
+        for (int i = 0; i < ssize(adj); i++)
+            if (sub_sz[i] == -1) dfs(i);
+    }
+    void calc_sz(int u, int p) {
+        sub_sz[u] = 1;
+        for (int v : adj[u])
+            if (v != p)
+                calc_sz(v, u), sub_sz[u] += sub_sz[v];
+    }
+    void dfs(int u) {
+        calc_sz(u, -1);
+        for (int p = -1, sz_root = sub_sz[u];;) {
+            auto big_ch = find_if(begin(adj[u]), end(adj[u]), [&](int v) {
+                return v != p && 2 * sub_sz[v] > sz_root;
+            });
+            if (big_ch == end(adj[u])) break;
+            p = u, u = *big_ch;
+        }
+        f(adj, u);
+        for (int v : adj[u]) {
+            iter_swap(find(begin(adj[v]), end(adj[v]), u), rbegin(adj[v]));
+            adj[v].pop_back();
+            dfs(v);
+        }
+    }
+};
