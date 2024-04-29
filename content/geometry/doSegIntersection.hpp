95c280dd817b358017b9c7a86c7bd65226fce1b2
Sachin S <ssk4988@gmail.com>
2024-04-29 16:40:57 -0400

    switched out spaces for tabs

--- content/geometry/doSegIntersection.hpp
+++ content/geometry/doSegIntersection.hpp
@@ -5,6 +5,6 @@
 #pragma once
 
 template<class P> bool doSegInter(P s1, P e1, P s2, P e2) {
-    return sideOf(s1, e1, s2) != sideOf(s1, e1, e2) && 
-           sideOf(s2, e2, s1) != sideOf(s2, e2, e1);
+	return sideOf(s1, e1, s2) != sideOf(s1, e1, e2) && 
+		   sideOf(s2, e2, s1) != sideOf(s2, e2, e1);
 }
--- content/strings/AhoCorasick-Tyler.h
+++ content/strings/AhoCorasick-Tyler.h
@@ -20,47 +20,47 @@
 const int ABSIZE = 26;
  
 struct node {
-    int nxt[ABSIZE];
-    vi ids = {};
-    int prv = -1, link = -1;
-    char c;
-    int linkMemo[ABSIZE];
+	int nxt[ABSIZE];
+	vi ids = {};
+	int prv = -1, link = -1;
+	char c;
+	int linkMemo[ABSIZE];
  
-    node(int prv = -1, char c = '$'): prv(prv), c(c) {
-        fill(all(nxt), -1);
-        fill(all(linkMemo), -1);
-    }
+	node(int prv = -1, char c = '$'): prv(prv), c(c) {
+		fill(all(nxt), -1);
+		fill(all(linkMemo), -1);
+	}
 };
  
 vector<node> trie(1);
  
 void addWord(string &s, int id) {
-    int cur = 0;
-    for(char c: s) {
-        int idx = c - 'a';
-        if(trie[cur].nxt[idx] == -1) {
-            trie[cur].nxt[idx] = sz(trie);
-            trie.emplace_back(cur, c);
-        }
-        cur = trie[cur].nxt[idx];
-    }
-    trie[cur].ids.push_back(id);
+	int cur = 0;
+	for(char c: s) {
+		int idx = c - 'a';
+		if(trie[cur].nxt[idx] == -1) {
+			trie[cur].nxt[idx] = sz(trie);
+			trie.emplace_back(cur, c);
+		}
+		cur = trie[cur].nxt[idx];
+	}
+	trie[cur].ids.push_back(id);
 }
  
 int getLink(int cur);
  
 int calc(int cur, char c) {
-    int idx = c - 'a';
-    auto &ret = trie[cur].linkMemo[idx];
-    if(ret != -1) return ret;
-    if(trie[cur].nxt[idx] != -1)
-        return ret = trie[cur].nxt[idx];
-    return ret = cur == 0 ? 0 : calc(getLink(cur), c);
+	int idx = c - 'a';
+	auto &ret = trie[cur].linkMemo[idx];
+	if(ret != -1) return ret;
+	if(trie[cur].nxt[idx] != -1)
+		return ret = trie[cur].nxt[idx];
+	return ret = cur == 0 ? 0 : calc(getLink(cur), c);
 }
  
 int getLink(int cur) {
-    auto &ret = trie[cur].link;
-    if(ret != -1) return ret;
-    if(cur == 0 || trie[cur].prv == 0) return ret = 0;
-    return ret = calc(getLink(trie[cur].prv), trie[cur].c);
+	auto &ret = trie[cur].link;
+	if(ret != -1) return ret;
+	if(cur == 0 || trie[cur].prv == 0) return ret = 0;
+	return ret = calc(getLink(trie[cur].prv), trie[cur].c);
 }
