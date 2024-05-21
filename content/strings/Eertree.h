ef202003ac60d478845ec650ff12cc50acf7d140
tylerm390 <77948992+tylerm390@users.noreply.github.com>
2024-05-21 11:25:27 -0400

    Made a few quick changes to formatting
    - Removed newpages to decrease page count
    - Switched eetree from using .size() to sz() macro
    - Changed ternary in hull tangents to remove warnings

--- content/strings/Eertree.h
+++ content/strings/Eertree.h
@@ -13,12 +13,12 @@ struct Eertree {
 	vvi down;
 	int cur = 0, t = 0;
 	Eertree(string &str) : down(2, vi(26, -1)) {
-		for (int i = 0; i < str.size(); i++) {
+		for (int i = 0; i < sz(str); i++) {
 			char c = str[i]; int ci = c - 'a';
 			while (t <= 0 || str[t-1] != c) 
 				t = i - len[cur = slink[cur]];
 			if (down[cur][ci] == -1) {
-				down[cur][ci] = slink.size();
+				down[cur][ci] = sz(slink);
 				down.emplace_back(26, -1);
 				len.push_back(len[cur] + 2);
 				if (len.back() > 1) {
@@ -26,7 +26,7 @@ struct Eertree {
 					while(t <= 0 || str[t-1] != c);
 					slink.push_back(down[cur][ci]);
 				} else slink.push_back(1);
-				cur = slink.size() - 1;
+				cur = sz(slink) - 1;
 			} else cur = down[cur][ci];
 			t = i - len[cur] + 1;
 		}
